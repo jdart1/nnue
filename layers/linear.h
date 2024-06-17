d721a83f591efaab83743637c09a31067e27320e
Jon Dart <chess1@jondart.name>
2024-06-17 16:12:02 -0700

    fix clang warning

--- layers/linear.h
+++ layers/linear.h
@@ -14,7 +14,7 @@ class LinearLayer : public TypedLayer<InputType, OutputType, inputSize, outputSi
     static constexpr size_t roundedInputSize = std::max<size_t>(32, inputSize);
 
   public:
-    LinearLayer() : _biases{0}, _weights{0} {
+    LinearLayer() : _biases{{0}}, _weights{{0}} {
     }
 
     virtual ~LinearLayer() = default;
