107ffa3d8ba72dfb95b9ef50f10de64a40c9f100
Jon Dart <chess1@jondart.name>
2024-10-16 07:23:13 -0700

    update params

--- nnparams.h
+++ nnparams.h
@@ -4,15 +4,15 @@
 
 struct NetworkParams {
 
-static constexpr unsigned KING_BUCKETS = 13;
+static constexpr unsigned KING_BUCKETS = 7;
 
 static constexpr unsigned OUTPUT_BUCKETS = 8;
 
-static constexpr unsigned HIDDEN_WIDTH = 1024;    
+static constexpr unsigned HIDDEN_WIDTH = 2048;
 
 // input quantization, i.e. 0..1 in float domain is
 // 0 .. NETWORK_QA in integer domain.
-static constexpr int NETWORK_QA = 255;
+static constexpr int NETWORK_QA = 510;
 // weight quantization, i.e. resolution of the weights. Weight of 1 in
 // integer domain has NETWORK_QB levels
 static constexpr int NETWORK_QB = 64;
@@ -21,14 +21,14 @@ static constexpr int OUTPUT_SCALE = 400;
 
 // clang-format off
 static constexpr unsigned KING_BUCKETS_MAP[] = {
-    0,  1,  2,  3,  3,  2,  1,  0,
-    4,  5,  6,  7,  7,  6,  5,  4,
-    8,  8,  9,  9,  9,  9,  8,  8,
-    10, 10, 10, 10, 10, 10, 10, 10,
-    11, 11, 11, 11, 11, 11, 11, 11,
-    11, 11, 11, 11, 11, 11, 11, 11,
-    12, 12, 12, 12, 12, 12, 12, 12,
-    12, 12, 12, 12, 12, 12, 12, 12};
+    0, 0, 1, 1, 1, 1, 0, 0,
+    2, 2, 2, 2, 2, 2, 2, 2,
+    3, 3, 3, 3, 3, 3, 3, 3,
+    4, 4, 4, 4, 4, 4, 4, 4,
+    5, 5, 5, 5, 5, 5, 5, 5,
+    5, 5, 5, 5, 5, 5, 5, 5,
+    6, 6, 6, 6, 6, 6, 6, 6,
+    6, 6, 6, 6, 6, 6, 6, 6};
 // clang-format on
 
 // version of the network
