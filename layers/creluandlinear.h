45b3a2b6a04add38ffd5f90841f908757522ed12
Jon Dart <chess1@jondart.name>
2024-11-02 10:25:49 -0700

    apply same fixes made to SqrCReLU layer

--- layers/creluandlinear.h
+++ layers/creluandlinear.h
@@ -7,9 +7,9 @@
 // This combines both a CReLU operation and a linear layer with output size one, the (single) hidden layer
 // in the Arasan V3 architecture.
 template <typename AccumulatorType, typename InputType, typename WeightType, typename BiasType, typename OutputType, size_t inputSize,
-          unsigned clampMax, size_t alignment = DEFAULT_ALIGN>
+          int clampMax, int NETWORK_QA, size_t buckets, size_t alignment = DEFAULT_ALIGN>
 class CReLUAndLinear
-    : public LinearLayer<InputType, WeightType, BiasType, OutputType, inputSize, 1, alignment> {
+    : public LinearLayer<InputType, WeightType, BiasType, OutputType, inputSize, 1, buckets, alignment> {
   public:
     CReLUAndLinear() = default;
 
@@ -20,49 +20,36 @@ class CReLUAndLinear
         assert(0);
     }
 
-    void postProcessAccum(const AccumulatorType &accum, OutputType *output) const {
+    void postProcessAccum(const AccumulatorType &accum, unsigned bucket, OutputType *output) const {
         int32_t sum = 0;
 #if defined(SIMD)
         if constexpr (sizeof(InputType) == 2) {
             simd::CRelUAndLinear < InputType, OutputType, WeightType, inputSize / 2, 1 >
                                       (accum.getOutput(AccumulatorHalf::Lower), output, clampMax,
-                                       this->_weights[0]);
+                                       this->_weights[bucket][0]);
             sum += *output;
             simd::CRelUAndLinear < InputType, OutputType, WeightType, inputSize / 2, 1 >
                                       (accum.getOutput(AccumulatorHalf::Upper), output, clampMax,
-                                       this->_weights[0] + inputSize / 2);
+                                       this->_weights[bucket][0] + inputSize / 2);
             sum += *output;
-            output[0] = (sum / NETWORK_QA) + this->_biases[0];
+            output[0] = (sum / NETWORK_QA) + this->_biases[bucket][0];
         } else
 #endif
         {
             // generic implementation
             static AccumulatorHalf halves[] = {AccumulatorHalf::Lower, AccumulatorHalf::Upper};
             size_t offset = 0;
-#ifdef NNUE_TRACE
-            std::cout << "stm weights ";
-            for (size_t i = 0; i < 20; ++i) {
-                std::cout << this->_weights[0][i] << ' ';
-            }
-            std::cout << std::endl;
-            std::cout << "opp side weights ";
-            for (size_t i = 0; i < 20; ++i) {
-                std::cout << this->_weights[0][i + accum.getSize()] << ' ';
-            }
-            std::cout << std::endl;
-
-#endif
             for (auto h : halves) {
                 for (size_t i = 0; i < accum.getSize(); ++i) {
                     int16_t x = accum.getOutput(h)[i];
                     // CReLU
                     x = std::clamp<int16_t>(x, 0, clampMax);
                     // multiply by weights and sum
-                    sum += (x * this->_weights[0][i + offset]);
+                    sum += (x * this->_weights[bucket][0][i + offset]);
                 }
                 offset += accum.getSize();
             }
-            output[0] = (sum / NETWORK_QA) + this->_biases[0];
+            output[0] = (sum / NETWORK_QA) + this->_biases[bucket][0];
         }
 #ifdef NNUE_TRACE
         std::cout << "---- CReLUAndLinear output " << std::endl;
