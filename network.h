24f2c9d2a8b86124c0474b6218137773c29f92a0
Jon Dart <chess1@jondart.name>
2025-03-05 12:46:57 -0800

    set transpose = true, for compatibility with current Arasan net

--- network.h
+++ network.h
@@ -25,9 +25,10 @@ class Network {
         NetworkParams::HIDDEN_WIDTH, NetworkParams::KING_BUCKETS_MAP>;
     using AccumulatorType = FeatureXformer::AccumulatorType;
     using AccumulatorOutputType = int16_t;
-    using OutputLayer = SqrCReLUAndLinear<AccumulatorType, int16_t, int16_t, int16_t, OutputType,
-                                          NetworkParams::HIDDEN_WIDTH * 2, NetworkParams::NETWORK_QA,
-                                          NetworkParams::NETWORK_QA, NetworkParams::OUTPUT_BUCKETS, true>;
+    using OutputLayer =
+        SqrCReLUAndLinear<AccumulatorType, int16_t, int16_t, int16_t, OutputType,
+                          NetworkParams::HIDDEN_WIDTH * 2, NetworkParams::NETWORK_QA,
+                          NetworkParams::NETWORK_QA, NetworkParams::OUTPUT_BUCKETS, true, true>;
 
     static constexpr size_t BUFFER_SIZE = 4096;
 
